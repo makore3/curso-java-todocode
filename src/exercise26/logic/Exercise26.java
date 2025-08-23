fcc0f35edd3be60fe0e404bf3d23e454ed699e3f
Samuel Cam <65782952+makore3@users.noreply.github.com>
2025-08-23 22:46:33 +0200

    Add Exercise26 project

---
+++ src/exercise26/logic/Exercise26.java
@@ -0,0 +1,13 @@
+package logic;
+
+import gui.Principal;
+
+public class Exercise26 {
+
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+}
